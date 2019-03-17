bdbab4d7626b313c41c427f9da4f0a17e71a4005
Ruslan <sabirov@worksolutions.ru>
2019-03-17 13:07:07 +0300

    DML: агрегация и сортировка. Расчет стоимости DAYSTAT

--- dml-group/script.sql
+++ dml-group/script.sql
@@ -1,5 +1,6 @@
 use voip;
 
+# Создаем временную таблицу
 create table tmp_oper_ip like oper_ip;
 insert into tmp_oper_ip
 select oper_ip.*
@@ -10,47 +11,26 @@ from oper_ip
   group by IP_OP
 ) v on v.vid = oper_ip.ID;
 
-select DST_NUMBER_IN,
-       DST_NUMBER_BILL,
-       BILL_DATE,
-       BILL_TIME,
-       ELAPSED_TIME,
-       SRC_IP,
-       ip_o.OP_ID   as                                       site_o,
-       s_o.SITENAME as                                       sname_o,
-       s_o.rate_o,
-       r_o.PRICE,
-       r_o.CODE_ID,
-       (select max(CODE)
-        from DEST_CODE dc
-        where dc.DEST_ID = r_o.CODE_ID
-          and dc.CODE in (
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 1),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 2),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 3),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 4),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 5)
-          )
-       )            as                                       max_code_o,
-       CAST(r_o.PRICE * ELAPSED_TIME / 60 AS DECIMAL(12, 6)) cost_per_minute_o,
-       DST_IP,
-       ip_t.OP_ID   as                                       site_t,
-       s_t.SITENAME as                                       sname_t,
-       r_t.PRICE,
-       r_t.CODE_ID,
-       (select max(CODE)
-        from DEST_CODE dc
-        where dc.DEST_ID = r_t.CODE_ID
-          and dc.CODE in (
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 1),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 2),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 3),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 4),
-                          SUBSTRING(c.DST_NUMBER_BILL, 1, 5)
-          )
-       )            as                                       max_code_t,
-       CAST(r_t.PRICE * ELAPSED_TIME / 60 as DECIMAL(12, 6)) cost_per_minute_t
 
+# Подсчет стоимости звонков DAYSTAT
+insert into DAYSTAT (BILL_DATE,
+                     TERM_OPER_ID,
+                     TERM_DEST_ID,
+                     TERM_COST,
+                     ORIG_OPER_ID,
+                     ORIG_DEST_ID,
+                     ORIG_COST,
+                     ALL_MIN,
+                     ALL_CALLS)
+select BILL_DATE,
+       ip_t.OP_ID             as                                  TERM_OPER_ID,
+       r_t.CODE_ID            as                                  TERM_DEST_ID,
+       SUM(CAST(r_t.PRICE * ELAPSED_TIME / 60 as DECIMAL(12, 4))) TERM_COST,
+       ip_o.OP_ID             as                                  ORIG_OPER_ID,
+       r_o.CODE_ID            as                                  ORIG_DEST_ID,
+       SUM(CAST(r_o.PRICE * ELAPSED_TIME / 60 AS DECIMAL(12, 4))) ORIG_COST,
+       SUM(ELAPSED_TIME) / 60 as                                  ALL_MIN,
+       COUNT(*)               as                                  ALL_CALLS
 from CDR as c
        inner join tmp_oper_ip ip_o on ip_o.IP_OP = c.SRC_IP
        inner join SITE as s_o on s_o.ID = ip_o.OP_ID
@@ -79,4 +59,9 @@ where (select max(CODE)
                          SUBSTRING(c.DST_NUMBER_BILL, 1, 4),
                          SUBSTRING(c.DST_NUMBER_BILL, 1, 5)
          )
-) is not null;
+) is not null
+group by c.BILL_DATE, ip_t.OP_ID, r_t.CODE_ID, ip_o.OP_ID, r_o.CODE_ID
+on duplicate key update DAYSTAT.ALL_CALLS=ALL_CALLS,
+                        DAYSTAT.ALL_MIN=ALL_MIN,
+                        DAYSTAT.TERM_COST=TERM_COST,
+                        DAYSTAT.ORIG_COST=ORIG_COST;
